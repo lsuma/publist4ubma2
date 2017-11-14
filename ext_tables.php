77b06367e12e67c844686cbc390ee8c31a05ba89
Linus Stubert <stubert@rz.uni-mannheim.de>
2017-11-14 15:36:42 +0000

    Changed icon to PNG format and TYPO3 v7+ style

--- ext_tables.php
+++ ext_tables.php
@@ -24,7 +24,7 @@ if (TYPO3_MODE === 'BE') {
 		),
 		array(
 			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ext_icon.gif',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ext_icon.png',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf',
 		)
 	);
